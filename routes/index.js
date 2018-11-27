d5eeaa90262362d6a8977683fe685645387d10ad
Takashi Abe <tabe@tabe-ltm2.internal.salesforce.com>
2018-11-27 17:13:50 +0900

    re-create Heroku Meetup #023 version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Salesforce 女子部 #19' });
+  res.render('index', { title: 'Heroku Meetup #23' });
 });
 
 module.exports = router;
