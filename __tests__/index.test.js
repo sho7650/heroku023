f256f0b54299fa0f98488f30251371df2c8d09a0
sho kisaragi <sho@oshiire.to>
2023-03-30 17:44:09 +0900

    add test code with supertest for routing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,39 @@
-test("two plus two is four", () => {
-  expect(2 + 2).toBe(4);
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+const router = require('../routes/index');
+
+app.set("view engine", "ejs");
+app.use('/', router);
+
+describe('Routing', () => {
+  test('GET / with localhost should render local template', async () => {
+    const response = await request(app).get('/').set('host', 'localhost');
+    expect(response.status).toBe(200);
+    expect(response.text).toContain('access to localhost');
+  });
+
+  test('GET / with prfm.info should render prfm template', async () => {
+    const response = await request(app).get('/').set('host', 'prfm.info');
+    expect(response.status).toBe(200);
+    expect(response.text).toContain('access to Perfume');
+  });
+
+  test('GET / with multi.kashiyuka.info should render kashiyuka template', async () => {
+    const response = await request(app).get('/').set('host', 'multi.kashiyuka.info');
+    expect(response.status).toBe(200);
+    expect(response.text).toContain('access to kashiyuka');
+  });
+
+  test('GET / with multi.prfm.jp should render index template', async () => {
+    const response = await request(app).get('/').set('host', 'multi.prfm.jp');
+    expect(response.status).toBe(200);
+    expect(response.text).toContain('access to prfm.jp');
+  });
+
+  test('GET / with unknown host should return 400 error', async () => {
+    const response = await request(app).get('/').set('host', 'unknown.host');
+    expect(response.status).toBe(400);
+  });
 });
---
+++ __tests__/users.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+const router = require('../routes/users');
+
+app.use('/', router);
+
+describe('GET /users', () => {
+  test('should return 200 and text "respond with a resource"', async () => {
+    const res = await request(app).get('/users');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('respond with a resource');
+  });
+});
